8496a00ef2b9fa643a673cab10f5bb54b056ed6b
Mahamood <shaikmisba305@gmail.com>
2024-12-12 14:36:02 +0530

    Add files via upload

---
+++ size.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String args[])
+    {
+        ArrayList<Integer>a=new ArrayList<>();
+        a.add(898);
+        a.add(89);
+        a.add(1);
+        a.add(78);
+        System.out.println(a.size());
+    }
+}
